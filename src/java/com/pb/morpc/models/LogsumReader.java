e77042adaa96813ec1426a81cca12a5b74f89346
wu sun <wsu@sandag.org>
2005-01-07 16:12:14 +0000

    initial import
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/morpc@276 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/morpc/models/LogsumReader.java
@@ -0,0 +1,39 @@
+/*
+ * Created on Dec 7, 2004
+ * Use this class to open a logsum table.
+ */
+package com.pb.morpc.models;
+
+import com.pb.common.util.ResourceUtil;
+import com.pb.common.datafile.TableDataSet;
+import com.pb.common.datafile.CSVFileReader;
+import java.util.HashMap;
+import java.util.logging.Logger;
+import java.io.IOException;
+;
+/**
+ * @author sunw
+ * <sunw@pbworld.com>
+ */
+public class LogsumReader {
+	
+    protected static HashMap propertyMap = ResourceUtil.getResourceBundleAsHashMap("morpc1");
+    protected static Logger logger = Logger.getLogger("com.pb.morpc.models");
+    protected static String logsumFile=(String)propertyMap.get("logsumFile");
+    protected static CSVFileReader reader;
+    protected static TableDataSet logsumTable;
+
+    static{
+    	reader=new CSVFileReader();
+    	try{
+    		logsumTable=reader.readTable(logsumFile);
+    	}catch(IOException e){
+    		logger.severe("failed opening logsum table:"+logsumFile);
+    	}
+    }
+    
+    public static TableDataSet getLogsumTable(){
+    	return logsumTable;
+    }
+
+}
