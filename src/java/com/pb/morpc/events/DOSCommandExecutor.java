7e34576cce79a7e0b960a71c597d2200ab606028
jim hicks <hicksji@pbworld.com>
2005-11-11 17:13:24 +0000

    Check in new project from Greg's laptop instead.
    git-svn-id: http://216.243.97.229:9090/svn/projects/trunk/morpc@863 ee1c2863-5742-f242-9f82-d2f51c100d09

---
+++ src/java/com/pb/morpc/events/DOSCommandExecutor.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright  2005 PB Consult Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+/*
+ * Created on Jul 26, 2004
+ * A call for executing DOD command from Java.
+ */
+
+package com.pb.morpc.events;
+
+/**
+ * @author Wu Sun Based on Jim Hick's MORPC codes
+ *	<sunw@pbworld.com>
+ */
+
+import org.apache.log4j.Logger;
+import java.util.HashMap;
+import com.pb.common.util.ResourceUtil;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DOSCommandExecutor {
+	
+    protected static Logger logger = Logger.getLogger(DOSCommandExecutor.class);
+	protected static HashMap propertyMap;
+	protected static String CMD_LOCATION;
+	
+	static {
+	    propertyMap = ResourceUtil.getResourceBundleAsHashMap("MiamiEvent");	
+		CMD_LOCATION = (String) propertyMap.get("CMD_LOCATION");
+	}
+	
+	public static HashMap getPropertyMap(){
+		return propertyMap;
+	}
+
+    public static void runDOSCommand(String command) {
+        try {
+            logger.info ("issuing DOS command: " + command);
+            sendCommand(command);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted exception ocurred for command: " + command);
+        }
+    }
+
+    public static void sendCommand(String command) throws InterruptedException {
+        try {
+
+            String s;
+            Process proc = Runtime.getRuntime().exec(CMD_LOCATION + "\\cmd.exe /C " + command);
+
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            BufferedReader stderr = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+            while ((s = stdout.readLine()) != null) {
+                logger.warn(s);
+            }
+
+            while ((s = stderr.readLine()) != null) {
+                logger.warn(s);
+            }
+        }
+        catch (IOException e) {
+            System.err.println("exception: " + e);
+        }
+    }
+}
---
+++ src/java/com/pb/morpc/events/TPPToBinarySkim.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright  2005 PB Consult Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+/*
+ * Created on Aug 4, 2004
+ * Use this class to convert TPP or TRANPLAN skims to binary skims.
+ */
+
+package com.pb.morpc.events;
+
+/**
+ * @author SunW based Jim Hick's codes
+ * <sunw@pbworld.com>
+ */
+
+import java.util.Vector;
+import java.util.HashMap;
+
+public class TPPToBinarySkim {
+	
+	protected HashMap propertyMap;
+	protected Vector skims;
+	protected String tppDir;
+	protected String binDir;
+	protected String TPP_TO_BINARY_PROGRAM_DIRECTORY;
+	protected String TPP_TO_BINARY_PROGRAM;
+	
+	public TPPToBinarySkim(HashMap propertyMap, String tppDir, String binDir){
+		this.propertyMap=propertyMap;	
+		this.tppDir=tppDir;
+		this.binDir=binDir;
+        TPP_TO_BINARY_PROGRAM_DIRECTORY = (String) propertyMap.get("TPP_TO_BINARY_PROGRAM_DIRECTORY");
+        TPP_TO_BINARY_PROGRAM = (String) propertyMap.get("TPP_TO_BINARY_PROGRAM");
+	}
+	
+	public void convert(){
+        DOSCommandExecutor.runDOSCommand(TPP_TO_BINARY_PROGRAM_DIRECTORY + "\\" + TPP_TO_BINARY_PROGRAM + " " + tppDir + " " + binDir);		
+	}
+}
